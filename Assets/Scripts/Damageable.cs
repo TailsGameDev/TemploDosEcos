1d10763e3f7d82b1dd231e7119a41fda6787bd53
Caina <cainacc@hotmail.com>
2021-01-28 13:56:12 -0300

    Damageable and Damager

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Damageable : MonoBehaviour
+{
+    [SerializeField]
+    private float life = 0;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        OnTriggerEnter2D(collision.collider);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Damager damager = collision.GetComponent<Damager>();
+        if (damager != null)
+        {
+            life -= damager.DamageAmount;
+
+            if (life <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Damager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damager : MonoBehaviour
+{
+    [SerializeField]
+    private float damageAmount = 0.0f;
+
+    public float DamageAmount { get => damageAmount; }
+}
